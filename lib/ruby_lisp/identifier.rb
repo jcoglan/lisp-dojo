dd669ef6ce999a9d1f73746d722e45fb7e7e9028
Daniel Lucraft <dan@fluentradical.com>
2011-03-22 19:10:50 +0000

    fuck yeah bindering and returning magical unifunctions

--- lib/ruby_lisp/identifier.rb
+++ lib/ruby_lisp/identifier.rb
@@ -1,6 +1,7 @@
 module RubyLisp::Identifier
   def eval(scope)
-    
+    return RubyLisp::Function.new if value == '+'
+    scope[value] or raise 'hell'
   end
 end
 
--- lib/ruby_lisp/scope.rb
+++ lib/ruby_lisp/scope.rb
@@ -1,5 +1,16 @@
 module RubyLisp
   class Scope
+    def []=(name, value)
+      bindering[name] = value
+    end
+    
+    def [](name)
+      bindering[name]
+    end
+    
+    def bindering
+      @binding ||= {}
+    end
   end
   
   class TopLevelScope < Scope
