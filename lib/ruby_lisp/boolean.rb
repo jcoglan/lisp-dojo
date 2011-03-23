ef1255ed348551fac84ad54d52f211c398dac812
James Coglan <jcoglan@gmail.com>
2011-03-23 10:24:53 +0000

    Eval booleans.

--- lib/ruby_lisp/boolean.rb
+++ lib/ruby_lisp/boolean.rb
@@ -1,6 +1,8 @@
 module RubyLisp::Boolean
   def eval(scope)
-  
+    # Booleans are either '#t' or '#f', so they should eval
+    # to true for '#t', false otherwise
+    value == '#t'
   end
 end
 
