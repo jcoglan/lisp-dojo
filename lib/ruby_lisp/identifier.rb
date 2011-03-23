c943508f96ce10c529e7ed26b8cbd4439375954a
James Coglan <jcoglan@gmail.com>
2011-03-23 10:29:10 +0000

    Add variable support.

--- lib/ruby_lisp/identifier.rb
+++ lib/ruby_lisp/identifier.rb
@@ -1,6 +1,8 @@
 module RubyLisp::Identifier
   def eval(scope)
-    
+    # Identifiers eval to whatever their name refers to
+    # in the current scope
+    scope[value]
   end
 end
 
--- lib/ruby_lisp/scope.rb
+++ lib/ruby_lisp/scope.rb
@@ -1,8 +1,30 @@
 module RubyLisp
   class Scope
+    def initialize
+      # A Scope's job is to store variables
+      @vars = {}
+    end
+    
+    # Backend for the 'define' keyword
+    def []=(name, value)
+      @vars[name] = value
+    end
+    
+    # Backend for resolving variable references
+    def [](name)
+      value = @vars[name]
+      raise "Undefined variable: #{name}" if value.nil?
+      value
+    end
   end
   
   class TopLevelScope < Scope
+    def initialize
+      super
+      # The top level is special, and defines the built-in functions
+      
+      self['+'] = Function.new
+    end
   end
 end
 
